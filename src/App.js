4fb293fa4b4025d06d82d4255baa9b0a5a18a55d
Lance Johnson <rlancejohnson@gmail.com>
2020-12-31 15:15:24 -0700

    Updated app cmp to support the add user form, hide scores button, and users list cmps

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import AddUserForm from './AddUserForm.js'
+import UsersList from './UsersList.js'
+import HideScoresButton from './HideScoresButton'
 
 export default class App extends Component {
+  state = {
+    users: [],
+    hideScores: false
+  }
+
+  addUser = (user) => {
+    this.setState((prevState) => ({
+      users: [...prevState.users, user]
+    }))
+  }
+
+  updateHideScores = (value) => {
+    this.setState({
+      hideScores: value
+    })
+  }
+
   render() {
+    const { users, hideScores } = this.state
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
+        <AddUserForm addUser={this.addUser}/>
+        <br/><br/>
+        <HideScoresButton hideScores={hideScores} updateHideScores={this.updateHideScores}/>
+        <UsersList users={users} hideScores={hideScores}/>
       </div>
-    );
+    )
   }
 }
